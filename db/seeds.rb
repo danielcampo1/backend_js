3ac18f8277489dbcaddc479c763502c34a3ec819
github username <github email address>
2021-03-18 16:53:29 -0500

    created category migration,seed, and table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+tech = Category.create(name:"tech")
+photography = Category.create(name:"photography")
+real_estate = Category.create(name: "real_estate")
