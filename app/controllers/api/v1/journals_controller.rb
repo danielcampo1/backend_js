228ce644c9f94f3aa60a01bbba0eaab2c3826062
github username <github email address>
2021-03-19 14:46:45 -0500

    created journal controller

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::JournalsController < ApplicationController
+    def index
+        journal = Journal.all
+        render json: JournalSerializer.new(journal)
+    end
+
+    def create
+        journal = Journal.new(journal_params)
+        if journal.save
+            render json: journal, status: :accepted
+        else
+            render json: {errors: journal.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+
+    private
+    def journal_params
+        params.require(:journal).permit(:title, :description, :image_url, :category_id)
+    end
 end
